941edcff65c731823134f3ef2b619350275cde59
Gonzalo Arce <gonza_arc@hotmail.com>
2024-03-02 18:04:13 -0300

    ok

--- scripts_tesis.js
+++ scripts_tesis.js
@@ -1,24 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const carousel = document.querySelector('.carousel');
-    const prevBtn = document.querySelector('.prev-btn');
-    const nextBtn = document.querySelector('.next-btn');
-    let currentIndex = 0;
-  
-    function showSlide(index) {
+  const carousel = document.querySelector('.carousel');
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  let currentIndex = 0;
+
+  function showSlide(index) {
       carousel.style.transform = `translateX(${-index * 100}%)`;
-    }
-  
-    function prevSlide() {
+  }
+
+  function prevSlide() {
       currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
       showSlide(currentIndex);
-    }
-  
-    function nextSlide() {
+  }
+
+  function nextSlide() {
       currentIndex = (currentIndex + 1) % carousel.children.length;
       showSlide(currentIndex);
-    }
-  
-    prevBtn.addEventListener('click', prevSlide);
-    nextBtn.addEventListener('click', nextSlide);
-  });
-  
+  }
+
+  function startAutoSlide() {
+      setInterval(function () {
+          nextSlide();
+      }, 5000); // Cambia de slide cada 5 segundos (5000 milisegundos)
+  }
+
+  // Agrega event listeners a los botones de "anterior" y "siguiente"
+  prevBtn.addEventListener('click', prevSlide);
+  nextBtn.addEventListener('click', nextSlide);
+
+  // Inicia automáticamente el cambio de slide cada 5 segundos
+  startAutoSlide();
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const attentionButton = document.getElementById('attentionButton');
+
+  function playBounceAnimation() {
+      attentionButton.classList.add('bounce-animation');
+  }
+
+  function startAutomaticAnimation() {
+      setInterval(function () {
+          attentionButton.classList.remove('bounce-animation');
+          void attentionButton.offsetWidth; // Triggers reflow to restart the animation
+          playBounceAnimation();
+      }, 3000); // Cambia la animación cada 3 segundos
+  }
+
+  // Inicia la animación automáticamente después de un breve retraso (por ejemplo, 1 segundo)
+  setTimeout(startAutomaticAnimation, 1000);
+});
